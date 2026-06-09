9defbd9f5288bfc7c88dab3d1500bf419f4450db
tomchccom <dreamkms2014@gmail.com>
2026-06-09 11:11:47 +0900

    chore : class -> className으로 변경

--- assignment-2/src/components/FilteringTabs.jsx
+++ assignment-2/src/components/FilteringTabs.jsx
@@ -1,7 +1,7 @@
 
 export default function FilteringTabs(){
     return (
-        <div class="filter-section">
+        <div className="filter-section">
             <button className="filter-btn active" data-filter="all">전체</button>
             <button className="filter-btn" data-filter="active">진행 중</button>
             <button className="filter-btn" data-filter="completed">완료</button>
